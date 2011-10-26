2072b4e4d698bd5bfa1425317df650fdfa4a32a6
gouqingsong <gouqingsong@336a2151-8ea9-148a-9f1a-241702e84fe1>
2011-10-26 13:36:53 +0000

    update imu module

---
+++ trunk/ufo-for-stm32/lib/imu/imufilter.h
@@ -0,0 +1,48 @@
+#ifndef _IMUFILTER_H
+#define _IMUFILTER_H
+/**
+ * @file imufilter.h
+ * IMU orientation filter.
+ */
+
+/**
+ *
+ */
+typedef struct orientation_t {
+    uint32_t pitch;
+    uint32_t roll;
+    uint32_t yaw;
+} orientation_t;
+    
+/**
+ * Compute the Euler angles based on the current filter data.
+ *
+ * @param orientation orientation to be updated
+ * @return 0 success, negative otherwise
+ */ 
+int IMUfilter_computeEnler(struct orientation_t *orientation)
+{
+}
+
+/**
+ * Get the current roll.
+ *
+ * @return The current roll angle in radians.
+ */
+ double getRoll(void);
+
+/**
+ * Get the current pitch.
+ *
+ * @return The current pitch angle in radians.
+ */
+double getPitch(void);
+
+/**
+ * Get the current yaw.
+ *
+ * @return The current yaw angle in radians.
+ */
+double getYaw(void);
+
+#endif /* _IMUFILTER_H */
