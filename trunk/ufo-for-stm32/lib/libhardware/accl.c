5916ae1d218ef513f74e0078cd8c58ac374d02d1
gouqingsong <gouqingsong@336a2151-8ea9-148a-9f1a-241702e84fe1>
2011-10-28 13:21:19 +0000

    update lib

---
+++ trunk/ufo-for-stm32/lib/libhardware/accl.c
@@ -0,0 +1,13 @@
+/**
+ * Get x, y, z axis raw data
+ *
+ * @param x the pointer to x-axis data
+ * @param y the pointer to y-axis data
+ * @param z the pointer to z-axis data
+ *
+ * @return 0 success, negative otherwise
+ */ 
+int accl_get_value(struct accl_event_t *event)
+{
+    return 0;
+}
