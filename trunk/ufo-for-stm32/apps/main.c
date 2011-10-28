875b2da89bf5a1cfe9ee8e066425bb5f4ddf48d7
gouqingsong <gouqingsong@336a2151-8ea9-148a-9f1a-241702e84fe1>
2011-10-28 12:38:43 +0000

    modify main.c

--- trunk/ufo-for-stm32/apps/main.c
+++ trunk/ufo-for-stm32/apps/main.c
@@ -1,3 +1,50 @@
+Accel_calibration(struct accel_offset *offset)
+{
+
+}
+
+#define SENSOR_TYPE_ACCL
+#define SENSOR_TYPE_GYRO
+#define SENSOR_TYPE_MARG
+#define SENSOR_TYPE_GPS
+
+typedef struct sensor_event_t
+    int type;
+    uint32_t x;
+    uint32_t y;
+    uint32_t z;
+} sensor_event_t;
+
+typedef struct accl_offset_t {
+    uint32_t accl_offset_x;
+    uint32_t accl_offset_y;
+    uint32_t accl_offset_z;
+} accl_offset_t;
+
+typedef struct gyro_bias_t {
+    uint32_t gyro_bias_x;
+    uint32_t gyro_bias_y;
+    uint32_t gyro_bias_z;
+} gyro_bias_t;
+
+Gyro_calibration(struct gyro_bias_t *bias)
+{
+    int buf_size = sizeof(gyro_buf) / 4;
+    
+    bias->gyro_bias_x = gyro_buf_x / buf_size;
+    bias->gyro_bias_y = gyro_buf_y / buf_size;
+    bias->gyro_bias_z = gyro_buf_z / buf_size;
+}
+    
+IMU_Init()
+{
+    /* hardware initialization */
+    Accel_init();
+    Gyro_init();
+    
+    Accel_calibration();
+    Gyro_calibration();
+}
 
 /**
  * global varible for accelerometer calibration
