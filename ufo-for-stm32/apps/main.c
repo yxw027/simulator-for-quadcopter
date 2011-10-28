10d6606cf267987ab512a0aeb56c5c5aad458a8c
gouqingsong <gouqingsong@336a2151-8ea9-148a-9f1a-241702e84fe1>
2011-10-28 12:32:06 +0000

    add folder apps

---
+++ ufo-for-stm32/apps/main.c
@@ -0,0 +1,15 @@
+
+/**
+ * global varible for accelerometer calibration
+ *
+ * gAccelOffsetX for x-axis
+ * gAccelOffsetY for y-axis
+ * gAccelOffsetZ for z-axis
+ *
+ * @note Not threadsafe
+ */
+u32 gAccelOffsetX = 0;
+u32 gAccelOffsetY = 0;
+u32 gAccelOffsetZ = 0;
+
+int check
