33015eeb19efe528805711063de2df5cf487fe5f
gouqingsong <gouqingsong@336a2151-8ea9-148a-9f1a-241702e84fe1>
2011-10-29 04:52:26 +0000

    rm folder library lib

--- ufo-rtt-stm32/lib/libhardware/gyro.c
+++
@@ -1,8 +0,0 @@
-gyro_calibration(struct gyro_bias_t *bias)
-{
-    int buf_size = sizeof(gyro_buf) / 4;
-    
-    bias->gyro_bias_x = gyro_buf_x / buf_size;
-    bias->gyro_bias_y = gyro_buf_y / buf_size;
-    bias->gyro_bias_z = gyro_buf_z / buf_size;
-}
