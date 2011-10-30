6d12bdd68c97fdd1cbecbda4c0d451a47293bd62
gouqingsong <gouqingsong@336a2151-8ea9-148a-9f1a-241702e84fe1>
2011-10-30 04:05:57 +0000

    add crc16 & misc bug fixing

---
+++ trunk/ufo-rtt-stm32/include/math/crc16.h
@@ -0,0 +1,30 @@
+/**
+ * @file crc16.h
+ * CRC-16 interface
+ */
+ 
+#ifndef _CRC16_H
+#define _CRC16_H
+
+/**
+ * Generator Polynomial
+ */
+#define CRC-16  0x8005  /* x^16 + x^15 + x^2 + 1 */
+
+/**
+ * compute the CRC-16 for the data buffer
+ *
+ * @param crc previous CRC value
+ * @param buffer data pointer
+ * @param len number of bytes in the buffer
+ * @return the updated CRC value
+ */
+uint16_t crc16(uint16_t crc, const uint8_t *buffer, int len);
+
+/** 
+ * @deprecated crc16 check only for VisualScope debug 
+ */
+unsigned short CRC_CHECK(unsigned char *Buf, unsigned char CRC_CNT);
+
+
+#endif /* _CRC16_H */ 
