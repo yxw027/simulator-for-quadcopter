77e957f2cc0d7e96966a7c0372c6ae8c7604d45d
gouqingsong <gouqingsong@336a2151-8ea9-148a-9f1a-241702e84fe1>
2011-11-02 05:33:39 +0000

    vector draft

--- ufo-rtt-stm32/include/math/kalman.h
+++ ufo-rtt-stm32/include/math/kalman.h
@@ -1,10 +1,23 @@
 /**
  * @file kalman.h
  * Kalman filter interface
+ * This file contains the code for a kalman filter, an
+ * extended kalman filter, and an iterated extended kalman filter.
  */
- 
+
 #ifndef _KALMAN_H
 #define _KALMAN_H
 
+typedef struct kalman_t {
+} kalman_t;
 
+/**
+ * Linear Kalman Filtering
+ */
+
+/**
+ * Extend Kalman Filtering
+ */
+
+ 
 #endif /* _KALMAN_H */
