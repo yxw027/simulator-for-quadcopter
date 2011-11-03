d6ff7de6063888ec8e46d7414ca98e9b96e11d3e
gouqingsong <gouqingsong@336a2151-8ea9-148a-9f1a-241702e84fe1>
2011-11-03 09:39:18 +0000

    add quaternion_to_euler support

--- ufo-rtt-stm32/include/math/quaternion.h
+++ ufo-rtt-stm32/include/math/quaternion.h
@@ -30,13 +30,33 @@ typedef struct estimated_quaternion_t {
 } estimated_quaternion_t;  
 
 /**
- * Get euluer from quaternion
+ * Get euler from quaternion
  *
  * @param quaternion pointer to quaternion
  * @param euler pointer to euluer
  * @return none 
  */
-void quaternion_to_euluer(struct quaternion_t *quaternion, struct euler_t *euler);
+void quaternion_to_euler(struct quaternion_t *quad, struct euler_t *euler);
+
+/**
+ * calculate quaternion from euler
+   w = c1 c2 c3 - s1 s2 s3
+   x = s1 s2 c3 + c1 c2 s3
+   y = s1 c2 c3 + c1 s2 s3
+   z = c1 s2 c3 - s1 c2 s3
+
+   where:
+        c1 = cos(heading / 2)
+        c2 = cos(attitude / 2)
+        c3 = cos(bank / 2)
+        s1 = sin(heading / 2)
+        s2 = sin(attitude / 2)
+        s3 = sin(bank / 2) 
+ * @param euler the original attitude
+ * @param quad quaternion from original euler
+ * @return none
+ */
+void quaternion_from_euler(struct euler_t *euler, struct quaternion_t *quad);
 
 /**
  * Normalize
