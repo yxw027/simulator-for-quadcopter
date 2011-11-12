245fdbd509da501a6c2be4eb8fd4a5543fa51446
gouqingsong <gouqingsong@336a2151-8ea9-148a-9f1a-241702e84fe1>
2011-11-12 07:31:23 +0000

    building ok, can not run, need some fixing

--- trunk/simulator/src/plugins/core/mainwindow.cpp
+++ trunk/simulator/src/plugins/core/mainwindow.cpp
@@ -1,8 +1,18 @@
 #include "mainwindow.h"
+
+#include <QtGui>
 using namespace Core;
 using namespace Core::Internal;
-MainWindow::MainWindow()
+
+MainWindow::MainWindow() :
+    EventFilteringMainWindow()
 {
+    setWindowTitle(tr("Simulator"));
+    
+    centralWidget = new QWidget(this);
+    setCentralWidget(centralWidget);
+
+    fileMenu = menuBar()->addMenu(tr("&File"));
 }
 
 MainWindow::~MainWindow()
@@ -11,12 +21,15 @@ MainWindow::~MainWindow()
 
 bool MainWindow::init(QString *errorMessage)
 {
+    Q_UNUSED(errorMessage)
+    //ExtensionSystem::PluginManager *pm = ExtensionSystem::PluginManager::instance();
     return true;
 }
 
 void MainWindow::extensionsInitialized()
 {
 }
+
 void MainWindow::aboutToShutdown()
 {
 }
