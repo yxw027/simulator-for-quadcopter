cc32aa5fce41e0f55689ce4f31df5316a7b4105e
gouqingsong <gouqingsong@336a2151-8ea9-148a-9f1a-241702e84fe1>
2011-11-15 06:51:42 +0000

    modify uart packet

--- trunk/ufo-rtt-stm32/modules/libhardware/uart.c
+++ trunk/ufo-rtt-stm32/modules/libhardware/uart.c
@@ -1,10 +1,12 @@
 /**
  * @file uart.c
- * uart modules implemtation
+ * uart modules implementation
  */
 
-#include "IUart.h"
+#include "hardware/IUart.h"
+#include "math/crc16.h"
 
+#include <stdio.h>
 
 /**
  * @addtogroup ufo
@@ -23,18 +25,18 @@ int uart_packet_tx(struct uart_packet_t *packet)
     int retval = 0;
     int index = 0;
     uint8_t buf[MAX_UART_LEN];
-    
+
     if (packet->type >= MAX_PACKET_TYPE)
         return retval;
     if (packet->length <= 0)
         return retval;
-   
+
     /* copy data from packet */
     buf[0] = packet->type;
     buf[1] = packet->length;
     for (i = 0, index = 2; i < packet->length; i++, index++)
         buf[index] = packet->buf[i];
-    buf[index] = packet->checksum;
+    buf[index++] = crc16(0, buf, index);
     
 #if UART_DEBUG
     printf("uart: data sent to uart:\n");
@@ -49,7 +51,26 @@ int uart_packet_tx(struct uart_packet_t *packet)
 
     /* start to transmit */
     for (i = 0; i < index; i++)
-        /* TODO send data via uart */
+        ;/* TODO send data via uart */
+}
+
+int gyro_packet_tx()
+{
+    struct uart_packet_t packet;
+
+    packet.type = GET_GYRO_DATA;
+    packet.length = 6;
+    /* x axis */
+    packet.buf[0] = 0x00;
+    packet.buf[1] = 0x00;
+    /* y axis */
+    packet.buf[2] = 0x00;
+    packet.buf[3] = 0x00;
+    /* z axis */
+    packet.buf[4] = 0x00;
+    packet.buf[5] = 0x00;
+
+    return uart_packet_tx(&packet);
 }
 
 /** @} */
