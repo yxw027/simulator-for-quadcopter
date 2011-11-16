cf43851c2710352d9b7be62a5cd749778d3bb166
gouqingsong <gouqingsong@336a2151-8ea9-148a-9f1a-241702e84fe1>
2011-11-16 08:03:41 +0000

    modify i2c interface

--- trunk/ufo-rtt-stm32/include/hardware/i2c.h
+++ trunk/ufo-rtt-stm32/include/hardware/i2c.h
@@ -24,11 +24,30 @@
 typedef struct i2c_dev_t {
     char *name;
     int addr;
-} i2c_dev_t ;
-    
+} i2c_dev_t;
+
 int i2c_probe(uint16_t addr);
-int i2c_read();
-int i2c_write();
+
+/**
+ * issue a sigle I2C message in master transmit mode
+ *
+ * @param addr The address of the slave
+ * @param buf Data will be written to the slave
+ * @param count How many bytes to write
+ * @return The number of bytes written if success, negative otherwise
+ */
+int i2c_write(uint16_t addr, const char *buf, int count);
+
+/**
+ * issue a sigle I2C message in master recive mode
+ *
+ * @param addr The address of the slave
+ * @param buf Where to store data read from slave 
+ * @param count How many bytes to read
+ * @return The number of bytes read if success, negative otherwise
+ */
+int i2c_read(uint16_t addr, char *buf, int count);
+
 
 /** @} */
 
