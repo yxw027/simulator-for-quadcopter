1dc17a25b01d1ab996d5fb6e69380f80d0970107
gouqingsong <gouqingsong@336a2151-8ea9-148a-9f1a-241702e84fe1>
2012-01-17 07:42:12 +0000

    modify AHRS

--- simulator/src/libs/ahrs/ahrs.h
+++ simulator/src/libs/ahrs/ahrs.h
@@ -1,7 +1,7 @@
 #ifndef _AHRS_H
 #define _AHRS_H
 
-class AHRS_EKF : public Kalman::EKFilter<double 7> {
+class AHRS_EKF : public Kalman::EKFilter<double, 7> {
 public:
     AhrsEKF();
     ~AhrsEKF();
@@ -10,13 +10,22 @@ protected:
     virtual void makeA();
     virtual void makeH();
     virtual void makeV();
+    virtual void makeR();
     virtual void makeW();
     virtual void makeQ();
     virtual void makeProcess();
     virtual void makeMeasure();
-
+/*
+    virtual void predict();
+    virtual void correct();
+    virtual void step();
+*/
 private:
-    //double ...
+    Vector gyro(3);
+    double measurement_noise_covariance;
 };
+
+typedef AHRS_EKF::Vector Vector;
+typedef AHRS_EKF::Matrix Matrix;
 
 #endif // _AHRS_H
