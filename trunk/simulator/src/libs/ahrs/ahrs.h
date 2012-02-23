4d00b53db28d406d27c48b7c4cc170d54ffca123
gouqingsong <gouqingsong@336a2151-8ea9-148a-9f1a-241702e84fe1>
2012-02-23 10:08:16 +0000

    modify ahrs

--- trunk/simulator/src/libs/ahrs/ahrs.h
+++ trunk/simulator/src/libs/ahrs/ahrs.h
@@ -1,7 +1,7 @@
 #ifndef _AHRS_H
 #define _AHRS_H
 
-class AHRS_EKF : public Kalman::EKFilter<double, 7> {
+class AHRS_EKF : public Kalman::EKFilter<double, 7, false, true, false> {
 public:
     AHRS_EKF();
     ~AHRS_EKF();
@@ -15,9 +15,19 @@ protected:
     virtual void makeQ();
     virtual void makeProcess();
     virtual void makeMeasure();
-
+    /**
+     * @brief Convert quaternion to direction-cosine-matrix
+     *
+     * \f[ DCM = \left [ \begin{array}{ccc}
+     *  1 - 2 *(q2^2 + q3^2) & 2*(q1q2 + q0q3) & 2*(q1q3 - q0q2 \\ \\
+     *  2 * (q1q2 - q0q3) & 1 - 2 * (q1^2 + q3^2) & 2 * (q2q3 + q0q1) \\ \\
+     *  2 * (q1q3 + q0q2) & 2 * (q2q3 - q0q1) & 1 - 2 * (q1^2 + q2^2) \\ \\
+     * \end{array} \right ] \f]
+     */
+    virtual void makeDCM();
 private:
     Vector gyro(3);
+    Matrix DCM(3, 3);
     double measurement_noise_covariance;
 };
 
