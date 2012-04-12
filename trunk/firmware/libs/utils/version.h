593f83ae1cddef1b47bbbbf2ffcc91ba40b8095d
gouqingsong <gouqingsong@336a2151-8ea9-148a-9f1a-241702e84fe1>
2012-04-12 14:25:42 +0000

    add utils/version.h

---
+++ trunk/firmware/libs/utils/version.h
@@ -0,0 +1,17 @@
+/**
+ * @file version.h
+ * @brief Version infomation
+ */
+
+#ifndef _VERSION_H
+#define _VERSION_H
+
+#define VER_MAJOR       0x00
+#define VER_MINOR       0x01
+
+
+#define VERSION    ((VER_MAJOR << 12) | (VER_MINOR << 8))
+
+
+#endif /* _VERSION_H */
+
