39f7b35cf08b2f64005a56c46d94d7a6499e2e67
jiangjirengood <jiangjirengood@gmail.com>
2012-05-15 13:22:13 +0000

    edit uart for printf

--- firmware/ahrs/bsp/stm32f10x/stm32f10x_uart.c
+++ firmware/ahrs/bsp/stm32f10x/stm32f10x_uart.c
@@ -1,65 +1,51 @@
 #include "../uart.h"
-
 #include <stm32f10x.h>
+#include <stdio.h>
 
+#define USART1_DR       (&(USART1->DR))
+#define USART1_BaudRate 9600           
 #define TxBufferSize1   1
-#define RxBufferSize1   1
 
 uint8_t TxBuffer1[TxBufferSize1];
-uint8_t RxBuffer1[RxBufferSize1];
 
 /**
-  * @brief  Configures the different system clocks.
+  * @brief  Configures the USART clock and pins .
   * @param  None
   * @retval None
   */
-static void RCC_Configuration(void)
+void USART1_Configuration(void)
 {
-    /* Enable DMA clock */
-    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
+  GPIO_InitTypeDef GPIO_InitStructure;
+  USART_InitTypeDef USART_InitStructure;
 
-    /* Enable GPIO clock */
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | USARTz_GPIO_CLK | RCC_APB2Periph_AFIO, ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_USART1 | RCC_APB2Periph_AFIO, ENABLE);
+  RCC_APB2PeriphResetCmd(RCC_APB2Periph_USART1, DISABLE);
 
-    /* Enable USART1 Clock */
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE); 
-}
+  /* Configure USART1 Tx (PA.09) as alternate function push-pull */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-/**
-  * @brief  Configures the different GPIO ports.
-  * @param  None
-  * @retval None
-  */
-static void GPIO_Configuration(void)
-{
-    GPIO_InitTypeDef GPIO_InitStructure;
+  /* Configure USART1 Rx (PA.10) as input floating */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+  
+  /*  USART1 configuration */
+  USART_InitStructure.USART_BaudRate = USART1_BaudRate;
+  USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+  USART_InitStructure.USART_StopBits = USART_StopBits_1;
+  USART_InitStructure.USART_Parity = USART_Parity_No;
+  USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+  USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
 
-    /* Configure USART1 Rx as input floating */
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-    /* Configure USART1 Tx as alternate function push-pull */
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
-}
-
-/**
-  * @brief  Configures the nested vectored interrupt controller.
-  * @param  None
-  * @retval None
-  */
-static void NVIC_Configuration(void)
-{
-    NVIC_InitTypeDef NVIC_InitStructure;
-
-    /* Enable the USART1 Interrupt */
-    NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-    NVIC_Init(&NVIC_InitStructure);
+  /* Configure USART1 */
+  USART_Init(USART1, &USART_InitStructure);
+  /* Enable the DMA to transmit data */
+  USART_DMACmd(USART1,USART_DMAReq_Tx,ENABLE);  
+  /* Enable the USART1 */
+  USART_Cmd(USART1, ENABLE);	
 }
 
 /**
@@ -71,9 +57,12 @@ static void DMA_Configuration(void)
 {
   DMA_InitTypeDef DMA_InitStructure;
 
-  /* USARTy_Tx_DMA_Channel (triggered by USARTy Tx event) Config */
-  DMA_DeInit(USARTy_Tx_DMA_Channel);
-  DMA_InitStructure.DMA_PeripheralBaseAddr = USARTy_DR_Base;
+  /* Enable DMA clocks */
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
+
+  /* USART1_Tx_DMA_Channel (triggered by USART1 Tx event) Config */
+  DMA_DeInit(DMA1_Channel4);
+  DMA_InitStructure.DMA_PeripheralBaseAddr = (u32)USART1_DR;
   DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)TxBuffer1;
   DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralDST;
   DMA_InitStructure.DMA_BufferSize = TxBufferSize1;
@@ -84,65 +73,35 @@ static void DMA_Configuration(void)
   DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;
   DMA_InitStructure.DMA_Priority = DMA_Priority_VeryHigh;
   DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
-  DMA_Init(USARTy_Tx_DMA_Channel, &DMA_InitStructure);
+  DMA_Init(DMA1_Channel4, &DMA_InitStructure);
   
-  /* USARTz_Tx_DMA_Channel (triggered by USARTz Tx event) Config */
-  DMA_DeInit(USARTz_Tx_DMA_Channel);
-  DMA_InitStructure.DMA_PeripheralBaseAddr = USARTz_DR_Base;
-  DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)TxBuffer2;
-  DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralDST;
-  DMA_InitStructure.DMA_BufferSize = TxBufferSize2;
-  DMA_Init(USARTz_Tx_DMA_Channel, &DMA_InitStructure);
 }
 
+/**
+  * @brief  Initial uart.
+  * @param  None
+  * @retval None
+  */
 void uart_init(void)
-{   /*!< At this stage the microcontroller clock setting is already configured, 
-       this is done through SystemInit() function which is called from startup
-       file (startup_stm32f10x_xx.s) before to branch to application main.
-       To reconfigure the default setting of SystemInit() function, refer to
-       system_stm32f10x.c file
-    */
-
-    USART_InitTypeDef USART_InitStructure;
-
-    /* System Clocks Configuration */
-    RCC_Configuration();
-
-    /* NVIC configuration */
-    NVIC_Configuration();
-
-    /* Configure the GPIO ports */
-    GPIO_Configuration();
-
-    /* Configure the DMA */
-    DMA_Configuration();
-
-/* USARTy and USARTz configuration -------------------------------------------*/
-    /* USARTy and USARTz configured as follow:
-        - BaudRate = 230400 baud  
-        - Word Length = 8 Bits
-        - One Stop Bit
-        - No parity
-        - Hardware flow control disabled (RTS and CTS signals)
-        - Receive and transmit enabled
-    */
-
-    USART_InitStructure.USART_BaudRate = 230400;
-    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-    USART_InitStructure.USART_StopBits = USART_StopBits_1;
-    USART_InitStructure.USART_Parity = USART_Parity_No;
-    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-    USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-
-    /* Configure USART1 */
-    USART_Init(USART1, &USART_InitStructure);
-
-    /* Enable USART1 DMA TX request */
-    USART_DMACmd(USART1, USART_DMAReq_Tx, ENABLE);
-
-    /* Enable USART1 */
-    USART_Cmd(USART1, ENABLE);
-
-    /* Enable USART1 DMA TX Channel */
-    DMA_Cmd(DMA1_Channel4, ENABLE);    
+{   
+  /* Configraue USART1 */
+	USART1_Configuration();
+  /* Configure USART1 TX DMA */        
+	DMA_Configuration();
+  /* Enable USART1 DMA TX Channel */
+  DMA_Cmd(DMA1_Channel4, ENABLE);    
 }
+
+int uart_send(int ch)
+{
+    DMA_Cmd(DMA1_Channel4, DISABLE);
+    TxBuffer1[0] = ch;
+    DMA_SetCurrDataCounter(DMA1_Channel4, TxBufferSize1);
+    DMA_Cmd(DMA1_Channel4, ENABLE);
+    while(DMA_GetFlagStatus(DMA1_FLAG_TC4) == RESET);
+    DMA_ClearFlag(DMA1_FLAG_TC4);
+
+    return ch;
+
+}
+
