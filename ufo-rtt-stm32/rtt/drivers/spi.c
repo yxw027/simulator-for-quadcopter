e03cc127c5460164bcd825b8f879961b8e79c89c
gouqingsong <gouqingsong@336a2151-8ea9-148a-9f1a-241702e84fe1>
2012-06-03 08:16:44 +0000

    update spi1

--- ufo-rtt-stm32/rtt/drivers/spi.c
+++ ufo-rtt-stm32/rtt/drivers/spi.c
@@ -7,8 +7,12 @@
   */
 static void RCC_Configuration(void)
 {
+    /* PCLK2 = HCLK/2 */
+    RCC_PCLK2Config(RCC_HCLK_Div2);
+
     /* Enable SPI_SPI_MASTER clock and GPIO clock */
-    RCC_APB2PeriphClockCmd(SPI_MASTER_GPIO_CLK | SPI_MASTER_CLK, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+    RCC_APB2PeriphClockCmd(SPI_MASTER_GPIO_CLK | SPI_MASTER_CLK/* | RCC_APB2Periph_AFIO*/, ENABLE);
 }
 
 /**
@@ -22,9 +26,12 @@ static void GPIO_Configuration(void)
 
     /* Disable the JTAG Port JTAG-DP & Enable SWJ-DP Port SWJ-DP */
     GPIO_PinRemapConfig(GPIO_Remap_SWJ_JTAGDisable, ENABLE);
+    // GPIO_PinRemapConfig(GPIO_Remap_SWJ_NoJTRST, ENABLE);
+    /* Enable SPI1 Pins Software Remapping */
+    GPIO_PinRemapConfig(GPIO_Remap_SPI1, ENABLE);
 
-    /* Configure NSS, SCK and MOSI pins as Alternate Function Push Pull */
-    GPIO_InitStructure.GPIO_Pin = /* SPI_MASTER_PIN_NSS | */SPI_MASTER_PIN_SCK | SPI_MASTER_PIN_MOSI;
+    /* Configure SCK and MOSI pins as Alternate Function Push Pull */
+    GPIO_InitStructure.GPIO_Pin = SPI_MASTER_PIN_SCK | SPI_MASTER_PIN_MOSI;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
     GPIO_Init(SPI_MASTER_GPIO, &GPIO_InitStructure);
@@ -33,10 +40,13 @@ static void GPIO_Configuration(void)
     GPIO_InitStructure.GPIO_Pin = SPI_MASTER_PIN_MISO;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
     GPIO_Init(SPI_MASTER_GPIO, &GPIO_InitStructure);
-    
-        GPIO_InitStructure.GPIO_Pin = SPI_MASTER_PIN_CS;
+
+    /* Configure CS pin */
+    GPIO_InitStructure.GPIO_Pin = SPI_MASTER_PIN_CS;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
     GPIO_Init(SPI_MASTER_GPIO, &GPIO_InitStructure);
+
+    GPIO_SetBits(GPIOA, GPIO_Pin_15);
 }
 
 void spi_init(void)
@@ -46,15 +56,15 @@ void spi_init(void)
     RCC_Configuration();
 
     GPIO_Configuration();
-
+    // SPI_Cmd(SPI1,DISABLE);
     /* SPI_MASTER configuration */
     SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
     SPI_InitStructure.SPI_Mode = SPI_Mode_Master;
     SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b;
     SPI_InitStructure.SPI_CPOL = SPI_CPOL_High;
-    SPI_InitStructure.SPI_CPHA = SPI_CPHA_1Edge;
+    SPI_InitStructure.SPI_CPHA = SPI_CPHA_2Edge;
     SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;
-    SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_4;
+    SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_8;
     SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
     SPI_InitStructure.SPI_CRCPolynomial = 7;
     SPI_Init(SPI_MASTER, &SPI_InitStructure);
