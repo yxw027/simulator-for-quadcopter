8c96ddb7e59f3c790c75daf986555b53b9fadd4b
gouqingsong <gouqingsong@336a2151-8ea9-148a-9f1a-241702e84fe1>
2013-01-22 13:25:03 +0000

    modify ahrs

--- trunk/firmware/ahrs/kalman/ahrs.c
+++ trunk/firmware/ahrs/kalman/ahrs.c
@@ -2,29 +2,41 @@
 
 #include "ekf.h"
 
-#define N 7
-#define M 
+#define N   7
+#define M   3
+#define NV
+#define NW
+
 struct kalman_data {
-	double u[3];
-	double dt;
-	double unused[3]; /* place holder */
+    double u[3];
+    double dt;
+    double unused[3]; /* place holder */
+    double z[3];
 };
 
 static void make_A(struct ekf *ekf)
 {
 //    struct kalman_data *data = (struct kalman_data *)ekf->data;
-    matrix_t *x = ekf->X;
+    matrix_t *X = ekf->X;
+    double q0 = M_INDEX(X, 0, 0);
+    double q0 = M_INDEX(X, 1, 0);
+    double q0 = M_INDEX(X, 2, 0);
+    double q0 = M_INDEX(X, 3, 0);    
+    double p = u[0] - M_INDEX(X, 4, 0);
+    double q = u[1] - M_INDEX(X, 5, 0);
+    double r = u[2] - M_INDEX(X, 6, 0);
+    
     double array[7][7] = {
-	{1, 0, 0, 0, 0, 0, 0},
-	{0, 1, 0, 0, 0, 0, 0},
-	{0, 0, 1, 0, 0, 0, 0},
-	{0, 0, 0, 1, 0, 0, 0},
-	{0, 0, 0, 0, 1, 0, 0},
-	{0, 0, 0, 0, 0, 1, 0},
-	{0, 0, 0, 0, 0, 0, 1},
+        { 0, -p, -q, -r,  q1,  q2,  q3 },
+        { p,  0,  r, -q,   0,   0,   0 },
+        { q, -r,  0,  p, -q3, -q0,  q1 },
+        { r,  q, -p,  0,  q2, -q1, -q0 },
+        { 0,  0,  0,  0,   0,   0,   0 },
+        { 0,  0,  0,  0,   0,   1,   0 },
+        { 0,  0,  0,  0,   0,   0,   1 },
     };
-
-    memcpy(x->data, array, x->rows * x->cols * sizeof(double));
+    memset(X->data, 0, X->rows * X->cols * sizeof(double));   //??????????????????
+    memcpy(X->data, array, X->rows * X->cols * sizeof(double)); //?????????????
 }
 
 static void make_H(struct ekf *ekf)
@@ -88,27 +100,28 @@ static void make_process(struct ekf *ekf)
     struct kalman_data *data = (struct kalman_data *)ekf->data;
     matrix_t *x = ekf->X;
     matrix_t *Xdot = matrix_new(N, 1);
-    if (Xdot) {
-        // TODO: type double is not suitable
-        double p = data->u[0] - M_INDEX(x, 4, 0);
-        double q = data->u[1] - M_INDEX(x, 5, 0);
-        double r = data->u[2] - M_INDEX(x, 6, 0);
+    if (!Xdot)
+        return;
+    // TODO: type double is not suitable
+    double p = data->u[0] - M_INDEX(x, 4, 0);
+    double q = data->u[1] - M_INDEX(x, 5, 0);
+    double r = data->u[2] - M_INDEX(x, 6, 0);
 
-        double q0 = M_INDEX(x, 0, 0);
-        double q1 = M_INDEX(x, 1, 0);
-        double q2 = M_INDEX(x, 2, 0);
-        double q3 = M_INDEX(x, 3, 0);
+    double q0 = M_INDEX(x, 0, 0);
+    double q1 = M_INDEX(x, 1, 0);
+    double q2 = M_INDEX(x, 2, 0);
+    double q3 = M_INDEX(x, 3, 0);
 
-        M_INDEX(Xdot, 0, 0) = (-p * q1 - q * q2 - r * q3) * data->dt / 2;
-        M_INDEX(Xdot, 1, 0) = (p * q0 - q * q3 + r * q2) * data->dt / 2;
-        M_INDEX(Xdot, 2, 0) = (p * q3 + q * q0 - r * q1) * data->dt / 2;
-        M_INDEX(Xdot, 3, 0) = (-p * q2 + q * q1 + r * q0) * data->dt / 2;
-        M_INDEX(Xdot, 4, 0) = 0;
-        M_INDEX(Xdot, 5, 0) = 0;
-        M_INDEX(Xdot, 6, 0) = 0;
+    M_INDEX(Xdot, 0, 0) = (-p * q1 - q * q2 - r * q3) * data->dt / 2;
+    M_INDEX(Xdot, 1, 0) = (p * q0 - q * q3 + r * q2) * data->dt / 2;
+    M_INDEX(Xdot, 2, 0) = (p * q3 + q * q0 - r * q1) * data->dt / 2;
+    M_INDEX(Xdot, 3, 0) = (-p * q2 + q * q1 + r * q0) * data->dt / 2;
+    M_INDEX(Xdot, 4, 0) = 0;
+    M_INDEX(Xdot, 5, 0) = 0;
+    M_INDEX(Xdot, 6, 0) = 0;
 
-        matrix_add(ekf->X, ekf->X, Xdot);
-    }
+    matrix_add(ekf->X, ekf->X, Xdot);
+
     matrix_delete(Xdot);
 }
 
@@ -122,28 +135,28 @@ static void callback(struct ekf *ekf)
 
 void ahrs_init(struct ekf *ekf, struct kalman_data *data)
 {
-	ekf_init(ekf, NULL, NULL);
+    ekf_init(ekf, NULL, NULL);
 
-	ekf->make_A = make_A;
-	ekf->make_H = make_H;
-	ekf->make_V = make_V;
-	ekf->make_R = make_R;
-	ekf->make_W = make_W;
-	ekf->make_Q = make_Q;
-	ekf->make_P = make_P;
-	ekf->make_process = make_process;
-	ekf->make_measure = make_measure;
-	ekf->callback = callback;
-	ekf->data = data;
+    ekf->make_A = make_A;
+    ekf->make_H = make_H;
+    ekf->make_V = make_V;
+    ekf->make_R = make_R;
+    ekf->make_W = make_W;
+    ekf->make_Q = make_Q;
+    ekf->make_P = make_P;
+    ekf->make_process = make_process;
+    ekf->make_measure = make_measure;
+    ekf->callback = callback;
+    ekf->data = data;
 }
 
 int main(int argc, char *argv[])
 {
-	struct ekf *ekf = ekf_new(7, 3, 0, 0);
-	struct kalman_data kdata;
+    struct ekf *ekf = ekf_new(7, 3, 0, 0);
+    struct kalman_data kdata;
 
-	ahrs_init(ekf, &kdata);
+    ahrs_init(ekf, &kdata);
     ekf_predict(ekf, kdata.u, kdata.dt);
     ekf_correct(ekf, kdata.unused);
-	return 0;
+    return 0;
 }
